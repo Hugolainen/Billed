0f0875e8d8596d678fa3291c1f41dc1ec39b43ca
hugoC42 <hugo.constant42@gmail.com>
2021-06-19 17:37:18 +0300

    Refractored bills tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,67 +1,61 @@
 import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import Bills from "../containers/Bills.js"
+import userEvent from "@testing-library/user-event";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+
 import { bills } from "../fixtures/bills.js"
 import { localStorageMock } from "../__mocks__/localStorage"
-import LoadingPage from "../views/LoadingPage.js";
-import ErrorPage from "../views/ErrorPage.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes";
-import firebase from "../__mocks__/firebase";
-import userEvent from "@testing-library/user-event";
+
 import firestore from '../app/Firestore'
 
-const mockBill = 
-[
-  {
-    id: "47qAXb6fIm2zOKkLzMro",
-    vat: "80",
-    fileUrl: "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
-    status: "pending",
-    type: "Hôtel et logement",
-    commentary: "séminaire billed",
-    name: "encore",
-    fileName: "preview-facture-free-201801-pdf-1.jpg",
-    date: "2004-04-04",
-    amount: 400,
-    commentAdmin: "ok",
-    email: "a@a",
-    pct: 20,
-  },
-]
+import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js"
+import LoadingPage from "../views/LoadingPage.js";
+import ErrorPage from "../views/ErrorPage.js";
 
 describe("Given I am connected as an employee", () => {
-  describe("when BillsUI is called", () => {
-    test("Then the Loading page should be rendered when loading = TRUE", () => {
-      const reference = LoadingPage();
-      const toTest = BillsUI({ data: bills, loading: true });
-      expect(toTest).toEqual(reference);
-    });
-
-    test("Then the Error page should be rendered when loading = FALSE & error = TRUE", () => {
-      const error = "Something went bad";
-      const reference = ErrorPage(error);
-      const toTest = BillsUI({ data: bills, loading: false, error});
-      expect(toTest).toEqual(reference);
-    });
-  });
+  describe("When BillsUI is called", () => {
+    describe("When the page is loading", () => {
+      test("Then the Loading page should be rendered", () => {
+        const reference = LoadingPage();
+        const toTest = BillsUI({ data: bills, loading: true });
+        expect(toTest).toEqual(reference);
+      })
+    })
+    describe("When the backend throws an error", () => {
+      test("Then the Error page should be rendered", () => {
+        const error = "Something went bad";
+        const reference = ErrorPage(error);
+        const toTest = BillsUI({ data: bills, loading: false, error});
+        expect(toTest).toEqual(reference);
+      })
+    })
+  })
   
   describe("When I am on Bills Page", () => {
     let onNavigate;
+    let container;
     beforeEach(() => {
         onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
-      }
+    }
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))
+       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+       window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))
+
+       container = new Bills({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
+      })
     })
 
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: bills})
       document.body.innerHTML = html
       expect(screen.getByTestId("icon-window").classList.contains("active-icon")).toBeTruthy;
-    });
-        
+    })
+      
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
@@ -69,52 +63,47 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
-    });
+    })
 
     describe("When I click on the NewBill button", () => {
-      test("Then the NewBill form should be rendered", () => {
+      test("Then the new bill form page should be rendered", () => {
         document.body.innerHTML = BillsUI({ data: bills });
-        onNavigate = jest.fn();
-        new Bills({
-          document,
-          onNavigate,
-          firestore,
-          localStorage,
-        });
         const newBill_btn = screen.getByTestId("btn-new-bill");
-        userEvent.click(newBill_btn);
-        expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
 
-        const data = [];
-        const loading = false;
-        const error = null;
-        const pathname = ROUTES_PATH["NewBill"];
-        const newBillHtml = ROUTES({ pathname, data, loading, error });
-        document.body.innerHTML  = newBillHtml;
+        onNavigate = jest.fn();
+        const formTrigger = jest.fn(container.handleClickNewBill);
+        
+        newBill_btn.addEventListener("click", formTrigger);
+        userEvent.click(newBill_btn);
+
+        expect(formTrigger).toHaveBeenCalled();
         expect(screen.getAllByText("Send a fee")).toBeTruthy();
-      });
-    });
+        expect(screen.getByTestId("form-new-bill") !== undefined).toBeTruthy();
+      })
+    })
 
     describe("When I click on the 'Eye' Icon of a bill", () => {
       test("Then the bill should be shown in a modal", () => {
-        $.fn.modal = jest.fn();
-        document.body.innerHTML = BillsUI({ data: mockBill });
-        const billsClass = new Bills({
-          document,
-          onNavigate,
-          firestore,
-          localStorage,
-        });
+        document.body.innerHTML = BillsUI({ data: bills });
+        const eyeIcon_btn = screen.getAllByTestId("icon-eye")[0];
+        //const eyeIcon_btn = eyeIcon_btnList[0];
 
-        const eyeIcon_btn = screen.getByTestId("icon-eye");
+        onNavigate = jest.fn();
+        const modaleTrigger = jest.fn(container.handleClickIconEye);
+
+        eyeIcon_btn.addEventListener("click", modaleTrigger(eyeIcon_btn));
         userEvent.click(eyeIcon_btn);
-        expect($.fn.modal).toHaveBeenCalledWith("show");
+        expect(modaleTrigger).toHaveBeenCalled();
 
-        const billImgModal = screen.getByText("Fee");
-        expect(billImgModal).toBeTruthy();
-      });
-    });
-  });
+        const modal = screen.getByTestId("modaleFile");
+        expect(modal).toBeTruthy();
 
-
+        const modalImageUrl = eyeIcon_btn.getAttribute("data-bill-url").split("?")[0];
+        expect(modal.innerHTML.includes(modalImageUrl)).toBeTruthy();
+        
+        const modalTitle = screen.getByText("Fee");
+        expect(modalTitle).toBeTruthy();
+      })
+    })
+  })
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,29 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import userEvent from "@testing-library/user-event"
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import { mockLocalStorage } from "../__mocks__/localStorage";
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    beforeEach(() => {
+
+    });
+
+    describe("When I upload an image", () => {
+      test("Then only images with .png, .jpg and .jpeg should be accepted", () => {
+
+      })
+    })
+    
+    
+    describe("When I completed the form and click on submit button", () => {
+      test("Then a new bill should be created and billsUI loaded", () => {
+
+      })
     })
   })
 })
