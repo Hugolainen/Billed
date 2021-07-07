d4058700a34c3da4d3ee8354b409dc421a0563d7
hugoC42 <hugo.constant42@gmail.com>
2021-07-07 19:33:36 +0300

    Corrected modal img size

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -24,7 +24,7 @@ export default class {
 
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
+    const imgWidth = $('#modaleFile').width()>750 ? "750px" : $('#modaleFile').width();
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     $('#modaleFile').modal('show')
   }
